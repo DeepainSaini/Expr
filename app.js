d53ac801bd82a5d530ee588c3f3d220682f8cf02
DeepainSaini <deepainsaini111@gmail.com>
2025-03-31 17:10:56 +0530

    Router and Filters

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express  = require('express');
 
 const app  = express();
 
+const productRouter = require('./routes/products');
+const categoryRouter = require('./routes/categories');
+
 const PORT = 4000;
 
 app.use((req,res,next)=>{
@@ -10,21 +13,8 @@ app.use((req,res,next)=>{
     next();
 });
 
-app.get('/products', (req, res) => {
-    res.send('Here is the list of all products.');
-});
-
-app.post('/products', (req, res) => {
-    res.send('A new product has been added.');
-});
-
-app.get('/categories', (req, res) => {
-    res.send('Here is the list of all the categories.');
-});
-
-app.post('/categories', (req, res) => {
-    res.send('A new category has been created.');
-});
+app.use(productRouter);
+app.use(categoryRouter);
 
 app.get('*',(req,res)=>{
     res.status(404).send('<h1>404 - page not found</h1>');
