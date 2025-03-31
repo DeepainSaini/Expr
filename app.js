258d95515de9862601cc3565e4af3287f5e2cfec
DeepainSaini <deepainsaini111@gmail.com>
2025-03-31 16:47:11 +0530

    Logging middleware

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express  = require('express');
 
 const app  = express();
 
-const PORT = 3000;
+const PORT = 4000;
+
+app.use((req,res,next)=>{
+    
+    console.log(`${req.method} request made to ${req.url}`)
+    next();
+});
 
 app.get('/products', (req, res) => {
     res.send('Here is the list of all products.');
