de9109c79f617c84c228ba8b9cc8ce6f3b056489
DeepainSaini <deepainsaini111@gmail.com>
2025-03-31 16:35:18 +0530

    Adding wildcard route

--- app.js
+++ app.js
@@ -4,22 +4,25 @@ const app  = express();
 
 const PORT = 3000;
 
-app.get('/orders', (req, res) => {
-    res.send('Here is the list of all orders.');
+app.get('/products', (req, res) => {
+    res.send('Here is the list of all products.');
 });
 
-app.post('/orders', (req, res) => {
-    res.send('A new order has been created.');
+app.post('/products', (req, res) => {
+    res.send('A new product has been added.');
 });
 
-app.get('/users', (req, res) => {
-    res.send('Here is the list of all users.');
+app.get('/categories', (req, res) => {
+    res.send('Here is the list of all the categories.');
 });
 
-app.post('/users', (req, res) => {
-    res.send('A new user has been added.');
+app.post('/categories', (req, res) => {
+    res.send('A new category has been created.');
 });
 
+app.get('*',(req,res)=>{
+    res.status(404).send('<h1>404 - page not found</h1>');
+});
 
 
 app.listen(PORT, () => {
